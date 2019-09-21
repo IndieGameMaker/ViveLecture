f0790edea77f422972a299207970c7e4a9b99ed1
Lee JaeHyun <myleje@gmail.com>
2019-09-21 13:57:03 +0900

    .

---
+++ Assets/02.Scripts/CastEventUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR.Extras;
+
+public class CastEventUI : MonoBehaviour
+{
+    private SteamVR_LaserPointer laserPointer;
+
+    void OnEnable()
+    {
+        laserPointer = GetComponent<SteamVR_LaserPointer>();
+
+        //이벤트 연결
+        laserPointer.PointerIn      += OnPointerEnter;
+        laserPointer.PointerOut     += OnPointerExit;
+        laserPointer.PointerClick   += OnPointerClick;
+    }
+
+    void OnDiable()
+    {
+        //이벤트 해지
+        laserPointer.PointerIn      -= OnPointerEnter;
+        laserPointer.PointerOut     -= OnPointerExit;
+        laserPointer.PointerClick   -= OnPointerClick;
+    }
+
+    void OnPointerEnter(object sender, PointerEventArgs e)
+    {
+        IPointerEnterHandler enter = e.target.GetComponent<IPointerEnterHandler>();
+        if (enter == null) return;
+
+        enter.OnPointerEnter(new PointerEventData(EventSystem.current));
+
+        /*
+        ExecuteEvents.Execute(e.target.gameObject
+                            , new PointerEventData(EventSystem.current)
+                            , ExecuteEvents.pointerEnterHandler);
+                            */
+    }
+
+    void OnPointerExit(object sender, PointerEventArgs e)
+    {
+        IPointerExitHandler enter = e.target.GetComponent<IPointerExitHandler>();
+        if (enter == null) return;
+
+        enter.OnPointerExit(new PointerEventData(EventSystem.current));        
+    }
+
+    void OnPointerClick(object sender, PointerEventArgs e)
+    {
+        IPointerClickHandler enter = e.target.GetComponent<IPointerClickHandler>();
+        if (enter == null) return;
+
+        enter.OnPointerClick(new PointerEventData(EventSystem.current));        
+    }
+
+}
---
+++ Assets/02.Scripts/ViveController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveController : MonoBehaviour
+{
+    public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+    public SteamVR_Input_Sources any = SteamVR_Input_Sources.Any;
+
+    //트리거 버튼
+    public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
+    //트랙패드 터치
+    public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackpadTouch;
+    //트랙패드의 위치(Vector2)
+    public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackpadPosition;
+    //그랩버튼
+    public SteamVR_Action_Boolean grabGrip = SteamVR_Actions.default_GrabGrip;
+    //헵틱
+    public SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
+
+    void Update()
+    {
+        if (trigger.GetStateDown(any))
+        {
+            Debug.Log("Left Trigger Clicked");
+        }
+
+        if (trackPadTouch.GetState(rightHand))
+        {
+            Vector2 pos = trackPadPosition.GetAxis(rightHand);
+            Debug.LogFormat("x={0}, y={1}" , pos.x, pos.y);
+        }
+
+        if (grabGrip.GetState(any))
+        {
+            Debug.Log("Grab Grip");
+            haptic.Execute(0.3f, 0.2f, 200.0f, 0.5f, leftHand);
+        }
+    }
+
+    public void OnClickButton(string msg)
+    {
+        Debug.Log("Click " + msg);
+    }
+}
