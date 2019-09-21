e4edcbafbacfddb70e5dd70310afaceb850262ea
Lee JaeHyun <myleje@gmail.com>
2019-09-21 16:17:22 +0900

    .

---
+++ Assets/02.Scripts/ButtonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    void OnEnable()
+    {
+        LaserPointer.OnPointerIn  += this.PointerIn;
+        LaserPointer.OnPointerOut += this.PointerOut;
+    }
+
+    void OnDisable()
+    {
+        LaserPointer.OnPointerIn  -= this.PointerIn;
+        LaserPointer.OnPointerOut -= this.PointerOut;
+    }    
+
+    void PointerIn(GameObject button)
+    {
+
+    }
+
+    void PointerOut(GameObject button)
+    {
+        
+    }
+
+}
